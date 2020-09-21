acf80bdee733dcdcac8b987af2545cf4e0f10afb
ygeslin <ygeslin@ygeslins-MacBook-Pro.local>
2020-09-21 20:15:07 +0200

    1st push

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#--------------------------- Variables ----------------------------------------#
+
+services=(				\
+			nginx		\
+			#ftps		\
+			#wordpress	\
+			#mysql		\
+			#phpmyadmin	\
+			#grafana	\
+			#influxdb	\
+)
+
+srcs=./srcs
+
+
+
+#--------------------------- Building contenairs ------------------------------#
+
+minikube start
+# add minikube env variables
+echo "adding minikube docker env\n"
+eval $(minikube docker-env)
+
+#for service in services "${services[@]}"
+docker build -t "$USER-nginx"	$srcs/nginx
+

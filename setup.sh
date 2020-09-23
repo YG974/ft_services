ee38c97f6006268a8dfd26f347b2f5bf01eca4d9
ygeslin <ygeslin@ygeslins-MacBook-Pro.local>
2020-09-23 21:17:54 +0200

    add manifest of metallb

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ services=(				\
 
 srcs=./srcs
 
+metallb_version="v0.9.3"
 
 
 #--------------------------- Building contenairs ------------------------------#
@@ -20,10 +21,21 @@ srcs=./srcs
 # add minikube env variables
 echo "adding minikube docker env\n"
 eval $(minikube -p minikube docker-env)
-#eval $(minikube docker-env)
+#minikube addons configure metallb eval $(minikube docker-env)
+minikube addons enable metallb
+minikube addons enable dashboard
+
+# How to install metallb
+# https://metallb.universe.tf/installation/
+kubectl apply -f
+https://raw.githubusercontent.com/metallb/metallb/$metallb_version/manifests/namespace.yaml
+kubectl apply -f
+https://raw.githubusercontent.com/metallb/metallb/$metall_version/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 
 #for service in services "${services[@]}"
 docker build -t "$USER-nginx" -f "$srcs/nginx/Dockerfile" srcs/nginx/.
-minikube addons enable dashboard
 
 

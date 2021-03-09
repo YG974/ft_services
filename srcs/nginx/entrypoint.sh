effccbefd1144d4d40adf666e2201a7efb7fc8f1
ygeslin <ygeslin@ygeslins-MacBook-Pro.local>
2021-03-09 21:06:05 +0100

    clean empty line

--- srcs/nginx/entrypoint.sh
+++ srcs/nginx/entrypoint.sh
@@ -10,8 +10,6 @@ openssl req -x509 \
 			-out /etc/ssl/certs/nginx-selfsigned.crt \
 			-subj "/C=FR/ST=Paris/L=Paris/O=42_School/OU=ygeslin/CN=ft_services"
 
-
 # standard command to start nginx with daemon off according to Nginx Docker doc :
 # https://github.com/nginxinc/docker-nginx/blob/594ce7a8bc26c85af88495ac94d5cd0096b306f7/mainline/buster/Dockerfile?fbclid=IwAR2H33H4e4KI9s6eMevQm0UNwcJa3Uzh7jVysJgyhtjgMfFZO7G1O4z1Dcc
  nginx -g 'daemon off;'
-

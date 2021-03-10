e90dd3a214274866c56987c2d640efb28e86d9fe
Yann GESLIN <ygeslin@e1r9p12.42.fr>
2021-03-10 15:09:57 +0100

    clean version avec exec

--- srcs/nginx/entrypoint.sh
+++ srcs/nginx/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 
 # https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-in-ubuntu-16-04
 # ssl
@@ -12,4 +12,6 @@ openssl req -x509 \
 
 # standard command to start nginx with daemon off according to Nginx Docker doc :
 # https://github.com/nginxinc/docker-nginx/blob/594ce7a8bc26c85af88495ac94d5cd0096b306f7/mainline/buster/Dockerfile?fbclid=IwAR2H33H4e4KI9s6eMevQm0UNwcJa3Uzh7jVysJgyhtjgMfFZO7G1O4z1Dcc
- nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
+# good practise to use exec, in order to have PID 1 on the service, to handle
+# UNIX Signal

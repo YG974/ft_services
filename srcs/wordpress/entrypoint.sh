7b83ead5d50afd4e7fc9688bae1889090a11be6b
Yann GESLIN <ygeslin@e2r5p8.42.fr>
2021-03-11 13:26:39 +0100

    wordpress seems to work, need to fulfil wp-config

--- srcs/wordpress/entrypoint.sh
+++ srcs/wordpress/entrypoint.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 
-# ssl etc
-
-# standard command to start nginx with daemon off according to Nginx Docker doc :
-# https://github.com/nginxinc/docker-nginx/blob/594ce7a8bc26c85af88495ac94d5cd0096b306f7/mainline/buster/Dockerfile?fbclid=IwAR2H33H4e4KI9s6eMevQm0UNwcJa3Uzh7jVysJgyhtjgMfFZO7G1O4z1Dcc
- nginx -g 'daemon off;'
+php -S 0.0.0.0:5050 -t /srv/wordpress
 

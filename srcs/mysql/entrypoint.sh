e13c4376ce64741bb31c1b03676ccfe76ccfc0a6
ygeslin <ygeslin@student.42.fr>
2021-04-07 19:28:29 +0200

    wordpress fonctionne avec docker network

--- srcs/mysql/entrypoint.sh
+++ srcs/mysql/entrypoint.sh
@@ -1,15 +1,16 @@
+#!/bin/ash
 
 openrc
 touch /run/openrc/softlevel
 
 /etc/init.d/mariadb setup
 
-# service mariadb restart
+service mariadb restart
 # sleep 1
 # to be able to communicate with other conteners
 sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
 
-# service mariadb restart
+service mariadb restart
 
 # DB_NAME="wp_db";
 # DB_USER="user";
@@ -17,7 +18,7 @@ sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
 # WP_ADMIN="admin";
 # WP_ADMIN_PASS="admin";
 
-mysql << EOF
+mysql --user=root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER "$DB_USER"@"%" IDENTIFIED BY "$DB_PASS";
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO "$DB_USER"@"%" WITH GRANT OPTION;
@@ -26,6 +27,6 @@ GRANT ALL PRIVILEGES ON $DB_NAME.* TO "$WP_ADMIN"@"%" WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 
-# mysql $DB_NAME < wordpress_db.sql
+mysql --user=root $DB_NAME < wordpress_db.sql
 
 tail -F /dev/null

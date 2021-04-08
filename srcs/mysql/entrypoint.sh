9020b463beac9c933f717cf7e143e99b002ee9fe
ygeslin <ygeslin@student.42.fr>
2021-04-08 22:03:35 +0200

    daskda

--- srcs/mysql/entrypoint.sh
+++ srcs/mysql/entrypoint.sh
@@ -6,17 +6,10 @@ touch /run/openrc/softlevel
 /etc/init.d/mariadb setup
 
 service mariadb restart
-# sleep 1
 # to be able to communicate with other conteners
 sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
 
 service mariadb restart
-
-# DB_NAME="wp_db";
-# DB_USER="user";
-# DB_PASS="user";
-# WP_ADMIN="admin";
-# WP_ADMIN_PASS="admin";
 
 mysql --user=root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
@@ -27,6 +20,7 @@ GRANT ALL PRIVILEGES ON $DB_NAME.* TO "$WP_ADMIN"@"%" WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 
- mysql --user=root $DB_NAME < newmysql.sql
+# dumping existing websibe
+ mysql --user=root $DB_NAME < wordpress_db.sql
 
 tail -F /dev/null
--- srcs/phpmyadmin/entrypoint.sh
+++ srcs/phpmyadmin/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/ash
 
-# PhpMyAdmin
-# MYSQL_IP=mysql;
-# sed -i "s/\'localhost\'/\'$MYSQL_IP\'/g" /etc/phpmyadmin/config.inc.php
-
 #launching openrc
 openrc
 touch /run/openrc/softlevel
@@ -13,9 +9,6 @@ chmod 555 /etc/phpmyadmin/config.inc.php
 
 service nginx start
 service php-fpm7 start
-
-# to run without fhp-fpm;
-#php -S 0.0.0.0:5000 -t /usr/share/webapps/phpmyadmin
 
 tail -F /dev/null
 

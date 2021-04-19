f940fba2c24f4360fb713b0f7bfde3741eee8a7f
ygeslin <ygeslin@student.42.fr>
2021-04-19 19:02:26 +0200

    change influxdb start by restart

--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -28,8 +28,8 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-// $cfg['Servers'][$i]['host'] = 'mysql';
-$cfg['Servers'][$i]['host'] = '172.17.0.2:3306';
+$cfg['Servers'][$i]['host'] = 'mysql';
+// $cfg['Servers'][$i]['host'] = '172.17.0.2:3306';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
